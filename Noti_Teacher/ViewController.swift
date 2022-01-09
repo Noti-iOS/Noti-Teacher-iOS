2818adcc5c13c8823e348fb0646754538f3bd2d5
Hwang <dudhkd98@naver.com>
2022-01-09 22:41:35 +0900

    Initial Commit

---
+++ Noti_Teacher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Noti_Teacher
+//
+//  Created by 황윤경 on 2022/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
