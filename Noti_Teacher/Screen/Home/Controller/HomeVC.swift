0fcaac0781f37a5300d14d507cab76fa16fedeb2
Hwang <dudhkd98@naver.com>
2022-01-12 17:11:59 +0900

    [FEAT]: NavigationBar Setting

--- Noti_Teacher/Screen/Home/Controller/HomeVC.swift
+++ Noti_Teacher/Screen/Home/Controller/HomeVC.swift
@@ -8,7 +8,28 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    @IBOutlet weak var customNaviBar: CustomNB!
     override func viewDidLoad() {
+        setNaviBar()
+    }
+}
+//MARK: Custom Function
+extension HomeVC {
+    func setNaviBar() {
+        customNaviBar.title = "TODAY'S HOMEWORK"
+        
+        customNaviBar.firstBtn.setImage(UIImage(named: "Alarm"), for: .normal)
+        customNaviBar.firstBtn.addTarget(self, action: #selector(showAlarm), for: .touchUpInside)
+        
+        customNaviBar.secondBtn.setImage(UIImage(named: "ETC"), for: .normal)
+        customNaviBar.secondBtn.addTarget(self, action: #selector(openSideMenu(_:)), for: .touchUpInside)
+    }
+    // alarm Btn
+    @objc func showAlarm() {
+        
+    }
+    // menu Btn
+    @objc func openSideMenu(_ sender: Any) {
         
     }
 }
